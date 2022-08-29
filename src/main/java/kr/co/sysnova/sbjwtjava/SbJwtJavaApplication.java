97530cb7623a476c0d692df02f4ca62e93265bf3
park won <parkwon@impconsulting.co.kr>
2022-08-29 20:05:03 +0900

    init

---
+++ src/main/java/kr/co/sysnova/sbjwtjava/SbJwtJavaApplication.java
@@ -0,0 +1,13 @@
+package kr.co.sysnova.sbjwtjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbJwtJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbJwtJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/sysnova/sbjwtjava/SbJwtJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.sysnova.sbjwtjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbJwtJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
